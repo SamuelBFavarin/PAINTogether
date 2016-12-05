a0c39eae4411c577d0f0b45ff5c516813eb82b3b
Lucas Baragatti <kronoxgames@gmail.com>
2016-12-05 18:15:19 -0200

    Converte projeto para Maven e implementa Serializador.

--- src/main/java/painTogether/ServerDispatcher.java
+++ src/main/java/painTogether/ServerDispatcher.java
@@ -1,6 +1,5 @@
 package painTogether;
 
-
 /**
  *
  * @author samuel
---
+++ src/main/java/painTogether/utils/Serializer.java
@@ -0,0 +1,22 @@
+package painTogether.utils;
+
+import com.google.gson.Gson;
+
+/**
+ * @author Lucas Baragatti
+ */
+public class Serializer {
+
+    public static <T> T fromJson(String json, Class<T> classType) {
+        Gson gson = new Gson();
+
+        return gson.fromJson(json, classType);
+    }
+
+    public static String toJson(Object object) {
+        Gson gson = new Gson();
+
+        return gson.toJson(object);
+    }
+
+}
