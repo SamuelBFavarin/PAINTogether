a3e81809954000a61579ddca13af261abe0673d9
Samuel Brat Favarin <samuelbfavarin@hotmail.com>
2016-12-05 20:38:10 -0200

    Set look and feel e ajustes na MainForm

--- src/main/java/PAINTogether/Client.java
+++ src/main/java/PAINTogether/Client.java
@@ -2,6 +2,8 @@ package PAINTogether;
 
 import PAINTogether.gui.Renderer;
 
+import javax.swing.*;
+
 /**
  *
  * @author samuel
@@ -9,6 +11,12 @@ import PAINTogether.gui.Renderer;
 public class Client {
     
     public static void main(String[] args) {
+        try {
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+        }
         new Renderer();
     }
     
--- src/main/java/PAINTogether/gui/MainForm.java
+++ src/main/java/PAINTogether/gui/MainForm.java
@@ -1,17 +1,7 @@
 package PAINTogether.gui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.HeadlessException;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  *
@@ -23,6 +13,7 @@ public class MainForm extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(300, 500);
         this.setLocationRelativeTo(null);
+        this.setMinimumSize(new Dimension(300, 500));
         
         this.setLayout(new BorderLayout());
         this.add(initTopLayout(),BorderLayout.NORTH);
@@ -33,7 +24,7 @@ public class MainForm extends JFrame {
     public JPanel initTopLayout(){
         JPanel topPanel = new JPanel();
         JLabel title = new JLabel("PAINTogether");
-        title.setFont(new Font("Arial", Font.BOLD, 32));;
+        title.setFont(new Font("Arial", Font.BOLD, 32));
         title.setForeground (Color.WHITE);
         topPanel.add(title);
         topPanel.setBackground(Color.DARK_GRAY);
@@ -42,17 +33,23 @@ public class MainForm extends JFrame {
     
     public JPanel initCenterLayout(){
         JPanel centerPanel = new JPanel();
+        JPanel enterPanel = new JPanel();
+
         JTextField txtField = new JTextField("Room Number");
-        txtField.setSize(20,50);
+        txtField.setMaximumSize(new Dimension(this.getWidth() * 100, 20));
+
         JButton btnEnter = new JButton("ENTER");
-        btnEnter.setSize(50, 100);
+        btnEnter.setMinimumSize(new Dimension(0, 0));
+
         JButton btnCreate = new JButton("Create Project");
         btnCreate.setSize(50, 100);
-        
-        centerPanel.setLayout(new BoxLayout(centerPanel,BoxLayout.PAGE_AXIS));
-        centerPanel.add(Box.createRigidArea(new Dimension(100, 100)));
-        centerPanel.add(txtField);
-        centerPanel.add(btnEnter);
+
+        enterPanel.add(txtField);
+        enterPanel.add(btnEnter);
+
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        centerPanel.add(Box.createRigidArea(new Dimension(20, 20)));
+        centerPanel.add(enterPanel);
         centerPanel.add(btnCreate);
         
         return centerPanel;
