b5153ae2fd01d90bbf3238b8d0ff59e934f22795
Vinícius Almeida dos Santos <e.viniciusas@gmail.com>
2016-12-12 20:58:10 -0200

    Criação inicial do servidor

--- server/server.js
+++ server/server.js
@@ -1,8 +1,37 @@
 var io = require('socket.io')(80);
 
+
+var rooms = {};
+var users = [];
+
 io.on('connection', function (socket) {
 
-    socket.on('disconnect', function () {
+    var myRoom = -1;
+    users.push(socket);
 
+    socket.on('disconnect', function () {
+        /// como remover um elemento especifico da lista javascript
+        rooms[room].
     });
+
+    socket.on('joinRoom', function(room){
+        if ( rooms[room] == undefined ){
+            rooms[room] = [];
+        }
+        if ( myRoom !== -1 ){
+            /// como remover um elemento especifico da lista javascript
+            rooms[room].
+        }
+        rooms[room].push(socket);
+        myRoom = room;
+    });
+
+    socket.on('draw', function(size,type,color,x,y){
+        if ( room === -1 ) return;
+        var actualRoom = rooms[myRoom];
+        for (var i = 0; i < actualRoom.length; i++){
+            actualRoom[i].emit(size,type,color,x,y);
+        }
+    });
+
 });
