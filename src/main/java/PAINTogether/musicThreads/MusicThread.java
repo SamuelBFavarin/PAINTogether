602fc190a3cc3f238cfb24eecaf2af33731cb766
Samuel Brat Favarin <samuelbfavarin@hotmail.com>
2016-12-19 15:17:11 -0200

    Loop para musica de background em .mp3

--- src/main/java/PAINTogether/musicThreads/MusicThread.java
+++ src/main/java/PAINTogether/musicThreads/MusicThread.java
@@ -2,24 +2,19 @@ package PAINTogether.musicThreads;
 
 import javazoom.jl.player.Player;
 
-import javax.sound.sampled.*;
-import java.io.File;
-import java.io.IOException;
-
+import java.io.FileInputStream;
 
 
 /**
  * Created by samuel on 14/12/16.
  */
 public class MusicThread extends Thread {
-    private static String assetsPath = "Assets/";
-    Clip clip = null;
-    private Player musicPlayer;
-    private AudioInputStream musicStream;
+
+    private Player music;
+    private String fileMusic = "Assets/Musics/music.mp3";
 
     @Override
     public void run() {
-        loadMusicStream();
         playMusic();
     }
 
@@ -29,52 +24,21 @@ public class MusicThread extends Thread {
         super.interrupt();
     }
 
-    private void loadMusicStream() {
-        try {
-            File file = new File(assetsPath + "/Musics/music.wav");
-            musicStream = AudioSystem.getAudioInputStream(file);
-
-        } catch (IOException | UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        }
-    }
-
     private void playMusic() {
-        /*
-        if (musicStream == null)
-            return;
         try {
-            if (musicPlayer == null)
-                musicPlayer = new Player(musicStream);
-
-            musicPlayer.play();
-            if (musicPlayer.isComplete()) {
-                musicPlayer.play();
-            }
-        } catch (JavaLayerException e) {
-            e.printStackTrace();
-        }*/
-
-        DataLine.Info info = new DataLine.Info(Clip.class, musicStream.getFormat());
-        try {
-            clip = (Clip) AudioSystem.getLine(info);
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
+            do {
+                FileInputStream buff = new FileInputStream(fileMusic);
+                music = new Player(buff);
+                music.play();
+            } while (true);
+        } catch (Exception e) {
+            e.fillInStackTrace();
         }
-        try {
-            try {
-                clip.open(musicStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        }
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
+
     }
 
     public void onInterrupted() {
-        if (musicPlayer != null)
-            musicPlayer.close();
+        if (music != null)
+            music.close();
     }
 }
